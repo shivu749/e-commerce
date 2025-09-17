eb62bb324940224407b063c9438a957ac5623c71
shivansht24 <167605875+shivansht24@users.noreply.github.com>
2025-09-17 22:03:11 +0530

    Initial commit: React e-commerce app with Vite and JSX fixes

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews(124)</div> 
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex illo sunt iusto sed tenetur ratione officiis veniam voluptatum nesciunt! Soluta, deserunt quasi. Officia autem explicabo quam maiores nulla veritatis dicta.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error magnam eos doloremque molestiae laudantium minus repudiandae ut aperiam rerum maiores, nostrum harum autem dolorem praesentium id dicta accusantium quae voluptates!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corporis quis, voluptas neque voluptate nobis unde aliquam ipsum provident repellat consequuntur minus aspernatur rerum, incidunt ullam dolor facere doloremque modi!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nobis doloribus dolores aliquid officia, excepturi in minus, accusantium consequatur vitae perspiciatis aliquam laborum eligendi blanditiis repudiandae tempora at esse quisquam!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis voluptas doloribus dolore enim. Aliquid voluptatum provident cumque quis sapiente sequi sint accusamus possimus. Fuga soluta odio numquam adipisci saepe?
+
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corporis quis, voluptas neque voluptate nobis unde aliquam ipsum provident repellat consequuntur minus aspernatur rerum, incidunt ullam dolor facere doloremque modi!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nobis doloribus dolores aliquid officia, excepturi in minus, accusantium consequatur vitae perspiciatis aliquam laborum eligendi blanditiis repudiandae tempora at esse quisquam!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis voluptas doloribus dolore enim. Aliquid voluptatum provident cumque quis sapiente sequi sint accusamus possimus. Fuga soluta odio numquam adipisci saepe?
+
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
